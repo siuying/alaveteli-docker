3aca0c1636dd711d3fb33190e5fba3899c6b4564
Francis Chong <francis@ignition.hk>
2016-09-18 11:49:37 +0800

    update the setup script to use thin

--- assets/setup.sh
+++ assets/setup.sh
@@ -19,7 +19,6 @@ mkdir -p $XAPIAN_MOUNT_PATH/$RAILS_ENV
 echo "linking $XAPIAN_MOUNT_PATH/$RAILS_ENV/ to /opt/alaveteli/lib/acts_as_xapian/xapiandbs/$RAILS_ENV/"
 ln -s $XAPIAN_MOUNT_PATH/$RAILS_ENV/ /opt/alaveteli/lib/acts_as_xapian/xapiandbs/$RAILS_ENV
 
-bundle exec rake db:create
 bundle exec rake db:migrate
 bundle exec rake themes:install
 bundle exec rake assets:precompile
@@ -29,4 +28,9 @@ cp -rf /opt/alaveteli/public /data/alaveteli
 
 chown -R $(whoami) /data
 
-bundle exec unicorn_rails -c ./config/unicorn.rb
+bundle exec thin \
+  --environment=$RAILS_ENV \
+  --user=alaveteli \
+  --group=alaveteli \
+  --servers=1 \
+  start
